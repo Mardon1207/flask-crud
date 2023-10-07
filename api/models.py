d828fa59a37ca439e1ed196ee92f06070eca3865
djumanov <diyorbekjumanov1120@gmail.com>
2023-10-07 17:46:24 +0500

    init

--- api/models.py
+++ api/models.py
@@ -1,1 +1,22 @@
 from tinydb import TinyDB
+
+
+class DB:
+    def __init__(self, file_name: str):
+        self.db = TinyDB(file_name, indent=4)
+        self.products = self.db.table('products')
+
+    def add_product(self, name, description, price, quantity):
+        pass
+
+    def get_product(self, id):
+        pass
+
+    def update_product(self, name, description, price, quantity):
+        pass
+
+    def delete_product(self, id):
+        pass
+    
+    def get_all_products(self):
+        pass
--- api/views.py
+++ api/views.py
@@ -4,6 +4,23 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return jsonify({"message": "ok"})
+@app.route('/api/v1/products/<int:product_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def product_view(product_id):
+    if request.method == 'GET':
+        # return product
+        pass
+    if request.method == 'POST':
+        # add new product
+        pass
+    if request.method == 'PUT':
+        # update product
+        pass
+    if request.method == 'DELETE':
+        # delete product
+        pass
+
+
+@app.route('/api/v1/products')
+def all_products_view():
+    # return all product
+    pass
